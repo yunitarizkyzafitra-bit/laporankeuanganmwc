30bb67efd50c51eb09d53e92dbeda68749f58e8e
yunitarizkyzafitra-bit <yunitarizkyzafitra@gmail.com>
2026-06-29 22:23:44 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Mengizinkan aplikasi berjalan online/offline dengan lancar
+});
